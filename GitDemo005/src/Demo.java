c5d84516540fe51127cce4a3bf21e79fef791a15
MANISH <MANISH@DESKTOP-NPV50CU>
2022-09-25 22:07:29 +0530

    chnages by cjc

---
+++ GitDemo005/src/Demo.java
@@ -0,0 +1,83 @@
+
+public class Demo
+{
+	
+	public void m1()
+	{
+		System.out.println(" M1 method ");
+		
+	}
+	
+	public void m2()
+	{
+		System.out.println(" M2 method ");
+		
+	}
+	
+	public void m3()
+	{
+		System.out.println(" M3 method ");
+		
+	}
+	
+	public void m4()
+	{
+		System.out.println(" M4 method ");
+		
+	}
+	
+	
+	public void sum()
+	{
+		System.out.println(" sum method ");
+		
+	}
+	
+	
+	public void mul()
+	{
+		System.out.println(" mul method ");
+		
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		
+		Demo d=new Demo();
+		d.m1();
+		d.m2();
+		
+		d.m3();
+		
+		d.m4();
+		
+		d.sum();
+		d.mul();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
